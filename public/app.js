0d51be6d97a9144dc086431d92b3265c1bdf65c5
Azizul Karim <karimazizul1@gmail.com>
2025-06-11 17:01:15 +0600

    init but with bugs

--- public/app.js
+++ public/app.js
@@ -2,11 +2,16 @@ const initialViewportWidth = window.innerWidth;
 
 const getPageLeft = () => window.visualViewport.pageLeft;
 
-const scrollToCenter = () => {
+// Assuming the **entire** body content starts from leftmost margin where left is 0,
+// snap the viewport to leftmost margin
+const snapToLeftmostMargin = () => window.scrollTo({ left: 0, behavior: 'smooth' });
+
+const scrollToLeftmostMargin = () => {
   // If page is scrolled by less than 8% (100-92) of viewport, scroll back to left = 0
   const threshold = initialViewportWidth - initialViewportWidth * 0.92;
-  if(getPageLeft() < threshold) {
+  if (getPageLeft() < threshold) {
     window.scrollTo({ left: 0, behavior: 'smooth' });
+    //snapToLeftmostMargin();
   }
 }
 
@@ -25,4 +30,14 @@ const setupScrollEndHandler = (callback) => {
     }
 }
 
-setupScrollEndHandler(scrollToCenter);
+const testOnClick = () => {
+  console.log("I have been clicked!");
+}
+
+setupScrollEndHandler(scrollToLeftmostMargin);
+
+// const onLoad = () => {
+//   document.body.addEventListener("click", (_) => testOnClick());
+// }
+
+// document.addEventListener("DOMContentLoaded", onLoad);
