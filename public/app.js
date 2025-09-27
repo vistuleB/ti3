5122e664abfe892bf4ded241d0bdc30aaa4780b9
Azizul Karim <karimazizul1@gmail.com>
2025-09-27 16:32:00 +0600

    fix merge conflicts

--- public/app.js
+++ public/app.js
@@ -65,7 +65,6 @@ const resetScreenWidthDependentVars = () => {
     root.style.setProperty(key, `${val()}` + unit);
   };
 
-<<<<<<< HEAD
   let inhaltsArrowsDisplay = () => {
     if (screenWidth <= LAPTOP_MAX_WIDTH) return "none";
     return "inline";
@@ -73,17 +72,6 @@ const resetScreenWidthDependentVars = () => {
 
   let menuPaddingXInRem = () => {
     if (screenWidth <= LAPTOP_MAX_WIDTH) return mainColumnPaddingXInRem();
-=======
-  let menuDisplay = () => {
-    if (screenWidth <= WELL_100VW_MINUS_PADDING_MAX_WIDTH) return "flex";
-    if (screenWidth <= MAIN_COLUMN_100VW_MAX_WIDTH) return "flex";
-    return "flex";
-  };
-
-  let menuPaddingXInRem = () => {
-    if (screenWidth <= MAIN_COLUMN_100VW_MAX_WIDTH)
-      return mainColumnPaddingXInRem();
->>>>>>> af3f1c7 (mobile menu refactor)
     return 1.7;
   };
 
@@ -94,12 +82,7 @@ const resetScreenWidthDependentVars = () => {
   };
 
   let menuBackgroundColor = () => {
-<<<<<<< HEAD
     if (screenWidth <= LAPTOP_MAX_WIDTH) return "var(--body-background-color)";
-=======
-    if (screenWidth <= MAIN_COLUMN_100VW_MAX_WIDTH)
-      return "var(--body-background-color)";
->>>>>>> af3f1c7 (mobile menu refactor)
     return "#0000";
   };
 
@@ -171,17 +154,9 @@ const resetScreenWidthDependentVars = () => {
   };
 
   const carouselNavButtonMarginXInPx = () => {
-<<<<<<< HEAD
     if (screenWidth <= MOBILE_MAX_WIDTH) return 0;
     if (screenWidth <= TABLET_MAX_WIDTH) return carouselArrowSizeInPx() * 0.7;
     if (screenWidth <= LAPTOP_MAX_WIDTH) return carouselArrowSizeInPx() * 0.4;
-=======
-    if (screenWidth <= WELL_100VW_MAX_WIDTH) return 0;
-    if (screenWidth <= WELL_100VW_MINUS_PADDING_MAX_WIDTH)
-      return carouselArrowSizeInPx() * 0.7;
-    if (screenWidth <= MAIN_COLUMN_100VW_MAX_WIDTH)
-      return carouselArrowSizeInPx() * 0.4;
->>>>>>> af3f1c7 (mobile menu refactor)
     return carouselArrowSizeInPx();
   };
 
@@ -239,15 +214,8 @@ const resetScreenWidthDependentVars = () => {
   };
 
   let topicAnnouncementFontSizeInRem = () => {
-<<<<<<< HEAD
     if (screenWidth <= MOBILE_MAX_WIDTH) return pageTitleFontSizeInRem() * 0.85;
     if (screenWidth <= TABLET_MAX_WIDTH) return pageTitleFontSizeInRem() * 0.82;
-=======
-    if (screenWidth <= WELL_100VW_MAX_WIDTH)
-      return pageTitleFontSizeInRem() * 0.85;
-    if (screenWidth <= WELL_100VW_MINUS_PADDING_MAX_WIDTH)
-      return pageTitleFontSizeInRem() * 0.82;
->>>>>>> af3f1c7 (mobile menu refactor)
     return pageTitleFontSizeInRem() * 0.78;
   };
 
@@ -913,7 +881,6 @@ const onScrollMenuDisplay = (_) => {
   const velocity =
     (currentScrollY - lastScrollY) / (currentScrollYMoment - lastScrollYMoment);
 
-<<<<<<< HEAD
   if (
     (velocity < -7 ||
       currentScrollY <= 10 ||
@@ -927,22 +894,6 @@ const onScrollMenuDisplay = (_) => {
     menuVisible()
   ) {
     setMenuVisibility(false);
-=======
-  if ((velocity < -7 || currentScrollY <= 10) && menuHidden) {
-    for (const m of menuElements) {
-      m.classList.remove("menu--hidden");
-    }
-    menuHidden = false;
-  } else if (
-    currentScrollY > lastScrollY &&
-    currentScrollY > 10 &&
-    !menuHidden
-  ) {
-    for (const m of menuElements) {
-      m.classList.add("menu--hidden");
-    }
-    menuHidden = true;
->>>>>>> af3f1c7 (mobile menu refactor)
   }
 
   lastScrollY = currentScrollY;
